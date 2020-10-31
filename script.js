5c5e9651b767fba7e3bfc63b5cc735ba10c9d8ac
Kristabell Johnson <bell77iam@gmail.com>
2020-10-31 01:27:54 -0600

    more html js and css

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var tableBody = document.getElementById('repo-table');
+var cardBody = document.getElementById('card-body');
 var fetchButton = document.getElementById('fetch-button');
-
+var
 function getApi() {
     // fetch request gets a list of all the repos for the node.js organization
     var requestUrl = 'api.openweathermap.org/data/2.5/forecast?id={city.list.json.gz}&appid={eccdeb80b86f2956956741aed8d0996a}';
-
+    console.log("getApi", getApi)
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
