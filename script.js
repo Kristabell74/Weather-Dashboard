da7b6e801fc6dccb204503783c37b44f0932ed93
Kristabell Johnson <bell77iam@gmail.com>
2020-11-01 13:08:23 -0700

    updated Js and HTml

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-var todaysDate = moment();
-$("#currentDay").text(todaysDate.format('MMMM Do YYYY, h:mm:ss a'));
-
 var cardBody = document.getElementById('card-body');
-var fetchButton = document.getElementById('fetch-button');
+//variables with the query Selectors
+var cityInput = document.querySelector('#searchBtn');
+var searchBtn = document.querySelector("#searchWeather")
+var userCitySpan = document.querySelector('#city')
+var citiesWeather = document.querySelector('.weather')
 
 function getApi() {
     // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = ('https://api.openweathermap.org/data/2.5/forecast?q=+city+&appid=eccdeb80b86f2956956741aed8d0996a')
-    console.log("getApi", getApi)
+    var city = cityInput.value;
+    var apiKey = "3c94a2bf570fbad6331f56c6025b394d"
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+    //fetching the response from the api
+    console.log("testing")
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data)
+
+            // var cityText = city.name
+            // document.getElementById("city").innerHTML = "City: " + cityText;
+            var tempText = data.list[0].main.temp;
+            document.getElementById("temperature").innerHTML = "Temperature: " + tempText;
+            var humidityText = data.list[0].main.humidity;
+            document.getElementById("humidity").innerHTML = "Humidity: " + humidityText;
+            var windText = data.list[0].main.wind;
+            document.getElementById("wind").innerHTML = "Wind: " + windText;
+
             //Loop over the data to generate a table, each table row will have a link to the repo url
             for (var i = 0; i < data.length; i++) {
                 // Creating elements, tablerow, tabledata, and anchor
@@ -34,49 +48,16 @@ function getApi() {
         });
 }
 
-//when the button is clicked to search it will fetch the weather
-fetchButton.addEventListener('click', getApi);
-//variables with the query Selectors
-var cityInput = document.querySelector('+city+');
-var searchBtn = document.querySelector(".Search")
-var userCitySpan = document.querySelector('#city')
-var citiesWeather = document.querySelector('.weather')
 
-renderLastRegistered();
-//display the message
-
+//Displays the city weather
 function displayMessage(type, message) {
     msgDiv.textContent = message;
     msgDiv.setAttribute("class", type);
 }
-function renderLastRegistered() {
-    var email = localStorage.getItem('+city+');
-    var searchBtn = localStorage.getItem('.search');
-    //if the search a city and it matches a city they show the weather
-    if (!search || !city) {
-        return (weather);
-
-    }
-
-    userCitySpan.textContent = city;
-
-}
-
 //event Listener to get the weather when click search with a city entered
-searchButton.addEventListener("click", function (event) {
+searchBtn.addEventListener("click", function (event) {
     event.preventDefault();
-
-    var city = document.querySelector(".city").value;
-    //search button will display weather if a city is entered
-    if (city == "searchBtn") {
-        displayWeather()
-        //if no city entered no information
-    } else if (searchBtn == "") {
-        displayMessage('"error" city name cannot be blank');
-        //local storage for city
-        localStorage.setItem("city", city);
-        renderLastRegistered();
-    }
+    getApi();
 
 
 });
